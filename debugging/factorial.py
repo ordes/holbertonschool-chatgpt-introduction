632555c412f7b5bbbd8ee486783ba329b092c8f1
ordes <10027@holbertonstudents.com>
2025-01-13 11:03:54 +0100

    factorial.py

---
+++ debugging/factorial.py
@@ -0,0 +1,11 @@
+import sys
+
+def factorial(n):
+    result = 1
+    while n > 1:
+        result *= n
+        n -= 1  # Decrement n to eventually exit the loop
+    return result
+
+f = factorial(int(sys.argv[1]))
+print(f)
